898d7b3dc18f31356376c42ee2311627350a6929
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2021-01-27 20:03:43 -0600

    Added Bootstrap Navbar
    Added botostrap to project. Created bootstrap navigation bar with working routing.
    
    Co-Authored-By: beverlycoronel <50159400+beverlycoronel@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-
+import Navigation from './components/Navigation'
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -7,36 +6,25 @@ import {
   Route,
   NavLink
 } from "react-router-dom";
-
 import About from './components/About'
 import Categories from './components/Categories'
 import Compare from './components/Compare'
 import Home from './components/Home'
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-      <header>
+      <Navigation>
+      </Navigation>
       <Router>
-        <nav>
-              <NavLink exact to ="/" activeClassName="selected"> Home </NavLink> | 
-              <NavLink to="/about" activeClassName="selected"> About </NavLink> |
-              <NavLink to="/categories" activeClassName="selected"> Categories </NavLink> | 
-              <NavLink to="/compare" activeClassName="selected"> Compare </NavLink>
-  
-        </nav>
-
-        <Switch>
-          <Route path="/about"><About /></Route>
+      <Switch>
+          <Route path="/about"><About/></Route>
           <Route path="/categories"><Categories /></Route>
           <Route path="/compare"><Compare /></Route>
-          <Route path="/"><Home /></Route> // Beverly's note: MUST BE AT BOTTOM, DO NOT MOVE
-        </Switch>
+          <Route path="/"><Home/></Route> // Beverly's note: MUST BE AT BOTTOM, DO NOT MOVE
+      </Switch>
       </Router>
-      </header>
-
     </div>
   );
 }
