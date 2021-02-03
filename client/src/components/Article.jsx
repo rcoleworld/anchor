4eacf10052d726464f71f183139e646896e39bf6
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2021-02-02 18:30:12 -0600

    Updated .js and .css for article view
    Reformatted how article is displayed in article view

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -4,8 +4,13 @@ import styles from '../stylesheets/articleview.css';
 
 const Article = (props) => {
     const article = props.location.state.article.props;
+    var date = article.date.replace('T00:00:00.000Z','').split("-");
+    date = new Date(date[0], date[1]-1, date[2]);
+    const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+    const month = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
+    const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+    date = month + " " + day + ", " + year
     var sentences = [ ]
-    console.log("article pls",article)
     article.body.map((body_info, index) => (sentences.push(body_info.sentence)))
         return (
             <div className="article">
@@ -16,13 +21,13 @@ const Article = (props) => {
                     From {article.source} | By {article.authors.map((author, index) => ( " " + author ))}
                 </div>
                 <div className="article-date">
-                    {article.date}
+                    {date}
                 </div>
                 <div>
                     <img className="article-image" src={article.thumbnail} alt = ""></img>
                 </div>
                 <div className="article-body">
-                    {sentences.map((sentence, index) => ( sentence ))}
+                    {sentences.map((sentence, index) => ( sentence + "" ))}
                 </div>
                 <div className="article-url">
                     <b>Find this article at: {article.url} </b>
