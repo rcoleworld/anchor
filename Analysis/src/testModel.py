82e2c2bcd135988ae56871f740229c8dc91dae4c
iCePU <polylewtonic@gmail.com>
2021-02-02 19:15:00 -0600

    removing command line spam
    This commit removes some spam that tensorflow sends to the command line

--- Analysis/src/testModel.py
+++ Analysis/src/testModel.py
@@ -13,8 +13,8 @@ import tensorflow_model_optimization as tfmot
 saved_model_path = "./_combinedBaisDetection"
 model_loaded = tf.keras.models.load_model(saved_model_path)
 
-examples = [ "abortion is a right that all women should have","capitalism is bad","Donald Trump was the greatest president that ever lived!"]
+examples = ["example string"]
 res = tf.sigmoid(model_loaded(tf.constant(examples)))
-model_loaded.summary()
+
 for i in range(len(examples)):
 	print(f"example: {examples[i]} Sentiment {res[i]}")
