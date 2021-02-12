9ba6dcb07fd88aad733b36ea9281983e5f78935f
beverlycoronel <bcoronel96@gmail.com>
2021-02-12 14:26:39 -0600

    Trending limited to 3 articles, articles ordered by recent date. Implemented hover animation

--- client/src/components/AritcleThumb.jsx
+++ client/src/components/AritcleThumb.jsx
@@ -2,20 +2,22 @@ import { Link } from 'react-router-dom';
 
 const ArticleThumb = (props) => {
         return (
+            <div className="default-box-container">
             <Link className= "default-box" to = {{
                 pathname: `${props.section}/${props.id}`,
                 state: {
                     article: {props}
                 }}}>
-                <div>
-                <div>
+                
+                
                     <img className= "default-thumbnail" src={props.thumbnail} alt = "nothing here"></img>
-                </div>
+                
                 <div className="default-title">
-                    {props.headline}
+                    <p id="headline">{props.headline}</p>
                 </div>
-                </div>
+                
                 </Link>
+            </div>
             
         )
 }
