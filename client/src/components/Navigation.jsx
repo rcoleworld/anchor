57d6da92f70bac9104b91cdba24e4bb1ffa108d2
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2021-02-12 13:33:21 -0600

    Updated navbar
    Completely removed bootstrap and its css that was affecting the navbar layout. Added dropdown menus for both compare and categories.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,24 +1,45 @@
-import React, { button } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 import '../stylesheets/navigationbar.css';
-
-const showSearchBar = () => {
-  var searchBar = document.getElementById("search-bar");
-  // searchBar.style.visibility == "visible";
-}
 
 const Navigation = () => {
   return (
-    <div className = "navbar">
-      <a href="/"><img className="navbar-logo" src="../images/anchor-light.png" alt = ""></img></a>
-      <ul>
-        <li><a href="/about" style={{ textDecoration: 'none' }}>About</a></li>
-        <li><a href="/categories" style={{ textDecoration: 'none' }}>Categories</a></li>
-        <li><a href="/compare" style={{ textDecoration: 'none' }}>Compare</a></li>
-        <li><button className="search-button"><img className="search-icon" src="../images/search.png" onClick={showSearchBar}></img></button></li>
-        <li><input className="search-bar" type="text" placeholder="search" /></li>
+    <nav className="navbar">
+      <a className="navbar-left" href="/">
+        <img className="navbar-logo" src="../images/anchor-light.png" alt = ""></img>
+      </a>
+      <ul className="navbar-right">
+        <NavItem text = "About" link = "/about"/>
+        <NavItem text = "Compare▾">
+          <div className="navbar-dropdown">
+            <a href="compare/sources" className="navbar-dropdown-item">Sources</a>
+            <a href="compare/articles" className="navbar-dropdown-item">Articles</a>
+          </div>
+        </NavItem>
+        <NavItem text = "Categories▾">
+          <div className="navbar-dropdown">
+            <a href="/category/politics" className="navbar-dropdown-item">Politics</a>
+            <a href="/category/world" className="navbar-dropdown-item">World</a>
+            <a href="/category/health" className="navbar-dropdown-item">Health</a>
+            <a href="/category/science" className="navbar-dropdown-item">Science</a>
+            <div className="navbar-horizontal-line"></div>
+            <a href="/categories" className="navbar-dropdown-item">View All</a>
+          </div>
+        </NavItem>
+        <input className = "navbar-search-bar" type="text" placeholder="Search"/>
+        <button className="navbar-search-button"><img className="navbar-search-icon" src="../images/search.png"></img></button>
       </ul>
-    </div>
-      );
-    }
+    </nav>
+  )
+}
+
+const NavItem = (props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <li className="navbar-item">
+      <a href={props.link} className="navbar-link" onClick={() => setOpen(!open)}>{props.text}</a>
+      {open && props.children}
+    </li>
+  );
+}
+
 export default Navigation;
