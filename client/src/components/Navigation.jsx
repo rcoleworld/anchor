a21156184b82ec92ff1e39ef00cfe9987d1e2f74
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2021-02-24 00:17:53 -0600

    Added light/dark/blue mode

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom';
+import{ useState } from "react";
 import '../stylesheets/navigationbar.css';
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const [theme, setTheme] = useState('blue');
+  const themeToggler = () => {
+    if (theme == 'blue') {
+      setTheme('light');
+      var passedTheme = 'light-theme'
+    }
+    else if (theme == 'light') {
+      setTheme('dark')
+      passedTheme = 'dark-theme'
+    }
+    else {
+      setTheme('blue')
+      passedTheme = 'blue-theme'
+    }
+    document.getElementById("image").src = "../images/bulb-" + theme + ".png";  
+    props.parentCallback(passedTheme);
+  }
+  var theme_image = "../images/bulb-" + theme + ".png"
   return (
     <nav className="navbar">
       <a className="navbar-left" href="/">
-        <img className="navbar-logo" src="../images/anchor-light.png" alt = ""></img>
+        <img id = "logo" className="navbar-logo" src="../images/anchor-light.png" alt=""></img>
       </a>
       <ul className="navbar-right">
         <li className="navbar-compare">
@@ -15,26 +34,27 @@ const Navigation = () => {
           <a href="/compare" className="navbar-link">Compare
             <img className="navbar-dropdown-arrow" src="../images/dropdown-arrow.png"></img>
           </a>
-              <div className="navbar-compare-dropdown">
-                <a href="compare/sources" className="navbar-dropdown-item">Sources</a>
-                <a href="compare/articles" className="navbar-dropdown-item">Articles</a>
-            </div>
+          <div className="navbar-compare-dropdown">
+            <a href="compare/sources" className="navbar-dropdown-item">Sources</a>
+            <a href="compare/articles" className="navbar-dropdown-item">Articles</a>
+          </div>
         </li>
         <li className="navbar-categories">
-        <a href="/categories" className="navbar-link">Categories
+          <a href="/categories" className="navbar-link">Categories
           <img className="navbar-dropdown-arrow" src="../images/dropdown-arrow.png"></img>
           </a>
           <div className="navbar-category-dropdown" >
-              <Link className="navbar-dropdown-item" to = {{pathname: "/politics", state: {category: "politics"}}}>Politics</Link>
-              <Link className="navbar-dropdown-item" to = {{pathname: "/world", state: {category: "world"}}}>World</Link>
-              <Link className="navbar-dropdown-item" to = {{pathname: "/health", state: {category: "health"}}}>Health</Link>
-              <Link className="navbar-dropdown-item" to = {{pathname: "/science", state: {category: "science"}}}>Science</Link>
+            <Link className="navbar-dropdown-item" to={{ pathname: "/politics", state: { category: "politics" } }}>Politics</Link>
+            <Link className="navbar-dropdown-item" to={{ pathname: "/world", state: { category: "world" } }}>World</Link>
+            <Link className="navbar-dropdown-item" to={{ pathname: "/health", state: { category: "health" } }}>Health</Link>
+            <Link className="navbar-dropdown-item" to={{ pathname: "/science", state: { category: "science" } }}>Science</Link>
             <div className="navbar-horizontal-line"></div>
-          <a href="/categories" className="navbar-dropdown-item">View All</a>
-        </div>
-      </li>
-        <input className = "navbar-search-bar" type="text" placeholder="Search"/>
+            <a href="/categories" className="navbar-dropdown-item">View All</a>
+          </div>
+        </li>
+        <input className="navbar-search-bar" type="text" placeholder="Search" />
         <button className="navbar-search-button"><img className="navbar-search-icon" src="../images/search.png"></img></button>
+        <img id="image" className="navbar-color-mode-icon" src={theme_image} onClick={themeToggler}></img>
       </ul>
     </nav>
   )
