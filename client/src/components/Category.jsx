aaba0378973af59fc2ab97b394a8400ecf2005d2
beverlycoronel <bcoronel96@gmail.com>
2021-02-26 06:21:35 -0600

    Fixed thumbnail image display

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -25,6 +25,7 @@ const Category = ()  => {
         <div className="category-page">
             <img className="category-image" src={image_source}/>
             <h1 className="category-title">{category}</h1>
+            <div className="recent-articles">
             <div className="articles">
                 {articles !== undefined && articles.length > 0 &&
                 articles.map((article, index) => (
@@ -44,6 +45,7 @@ const Category = ()  => {
                 ></ArticleThumb>
                 ))
             }
+            </div>
             </div>
     </div>
     )
