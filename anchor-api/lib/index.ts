8d4baacc8e3e4ec97817533d958b2fa7b40c17ba
Reginald Thomas <reginaldcolethomas@gmail.com>
2021-03-16 22:47:38 -0500

    Added ability to get average objectivity, sentiment, and bias of all fields.

--- anchor-api/lib/index.ts
+++ anchor-api/lib/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import Article from './models/article.model';
 import { getArticles } from './controllers/getArticles';
-import {handleWebScraper } from './controllers/handleWebScraper';
+import {getAverageStats} from './controllers/getStats';
+import { handleWebScraper } from './controllers/handleWebScraper';
 import { searchArticles } from './controllers/searchArticles';
 import dotenv from 'dotenv';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 dotenv.config();
@@ -21,7 +22,7 @@ app.use(cors());
 
 app.post('/articles', handleWebScraper);
 app.get('/articles', getArticles);
-app.get(['/articles/search/:searchString','/articles/search'], searchArticles);
-
+app.get(['/articles/search/:searchString', '/articles/search'], searchArticles);
+app.get('/stats/:field', getAverageStats);
 app.listen(PORT, HOST);
 console.log(`Listening on ${HOST}:${PORT}`);
