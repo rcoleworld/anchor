154ebc83659591c0b2eafed6818e419fc9a40f50
PromyseWard <55416601+PromyseWard@users.noreply.github.com>
2021-05-04 21:18:36 -0500

    Updated article loading

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -2,16 +2,215 @@ import { withRouter } from "react-router-dom";
 import { useState } from "react";
 import "../stylesheets/articleview.css";
 import SpectrumBar from "./SpectrumBar";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const { REACT_APP_SERVER_URL } = process.env;
+
+const defaultArticle = {
+  "authors": [
+    "Alexandria Hein",
+    "David Aaro"
+],
+"id": "607d9d1e7a348f0020558c55",
+"url": "https://www.foxnews.com/health/coronavirus-in-us-state-by-state-breakdown",
+"date": "2020-05-07T12:09:33.000Z",
+"headline": "Coronavirus in the US: State-by-state breakdown",
+"section": "health",
+"thumbnail": "https://cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/162d324d-978e-4e4b-8905-05a179879fae/e6089be8-5b7f-43f8-87dc-da86fa21a95b/1280x720/match/image.jpg",
+"body": [
+    [
+        "Get all the latest news on coronavirus and more delivered daily to your inbox.",
+        0.9980278015136719,
+        0.9704023599624634,
+        0.562663197517395
+    ],
+    [
+        "Sign up here.",
+        0.9972244501113892,
+        0.9742332696914673,
+        0.5671975016593933
+    ],
+    [
+        "As of Monday morning, the novel coronavirus has infected more than 141,438,278 people across 192 countries and territories, resulting in at least 3,020,688 deaths.",
+        0.9950703382492065,
+        0.676948070526123,
+        0.5788543224334717
+    ],
+    [
+        "In the U.S., all 50 states plus the District of Columbia have reported confirmed cases of COVID-19, tallying more than 31,670,041 illnesses and at least 567,217 deaths.",
+        0.9992283582687378,
+        0.9025897979736328,
+        0.5597905516624451
+    ],
+    [
+        "Here's a look at the 10 states with the most cases: \n1.",
+        0.9994722604751587,
+        0.9537893533706665,
+        0.5483156442642212
+    ],
+    [
+        "California: 3,718,210 cases\n2.",
+        0.999542772769928,
+        0.9536205530166626,
+        0.541312038898468
+    ],
+    [
+        "Texas: 2,850,575 cases\n3.",
+        0.9995055198669434,
+        0.9666938781738281,
+        0.5393960475921631
+    ],
+    [
+        "Florida: 2,168,901 cases \n4.",
+        0.999521017074585,
+        0.9714161157608032,
+        0.5401296019554138
+    ],
+    [
+        "New York: 1,998,912 cases\n5.",
+        0.9995355606079102,
+        0.971526026725769,
+        0.540031909942627
+    ],
+    [
+        "Illinois: 1,302,134 cases \n6.",
+        0.9995343685150146,
+        0.9735642671585083,
+        0.5408903956413269
+    ],
+    [
+        "Pennsylvania: 1,107,673 cases\n7.",
+        0.9993528127670288,
+        0.9818515777587891,
+        0.5407072305679321
+    ],
+    [
+        "Georgia: 1,084,272 cases\n8.",
+        0.9993060827255249,
+        0.9835301637649536,
+        0.5439324975013733
+    ],
+    [
+        "Ohio: 1,053,175 cases\n9.",
+        0.9992837309837341,
+        0.9838943481445312,
+        0.5457758903503418
+    ],
+    [
+        "New Jersey: 978,853 cases \n10.",
+        0.9992649555206299,
+        0.9852631092071533,
+        0.5486589670181274
+    ],
+    [
+        "North Carolina: 943,693 cases \nHere's a look at the top 10 states with the most deaths: \n1.",
+        0.9995462894439697,
+        0.9959479570388794,
+        0.5354756116867065
+    ],
+    [
+        "California: 61,038 deaths \n2.",
+        0.9994642734527588,
+        0.9954177141189575,
+        0.5392836928367615
+    ],
+    [
+        "New York: 51,579 deaths \n3.",
+        0.9994661808013916,
+        0.9952424764633179,
+        0.5407531261444092
+    ],
+    [
+        "Texas: 49,595 deaths\n4.",
+        0.9994430541992188,
+        0.9951856136322021,
+        0.5404790043830872
+    ],
+    [
+        "Florida: 34,439 deaths \n5.",
+        0.9994434118270874,
+        0.9950944185256958,
+        0.5407685041427612
+    ],
+    [
+        "Pennsylvania: 25,665 deaths\n6.",
+        0.9994167685508728,
+        0.994830846786499,
+        0.5421783924102783
+    ],
+    [
+        "New Jersey: 25,143 deaths\n7.",
+        0.9997013807296753,
+        0.9923407435417175,
+        0.5364347696304321
+    ],
+    [
+        "Illinois: 23,955 deaths\n8.",
+        0.9996813535690308,
+        0.9919160604476929,
+        0.5367944240570068
+    ],
+    [
+        "Georgia: 19,758 deaths\n9.",
+        0.999649167060852,
+        0.991830050945282,
+        0.53590327501297
+    ],
+    [
+        "Ohio: 18,991 deaths\n10.",
+        0.9996229410171509,
+        0.9916603565216064,
+        0.5356607437133789
+    ],
+    [
+        "Michigan: 17,934 deaths\nThis file will be updated regularly.",
+        0.9997654557228088,
+        0.9934064149856567,
+        0.5394259691238403
+    ]
+],
+"category": "health",
+"source": "Fox News",
+"bias": 0.9991628122329712,
+"sentiment": 0.9672878217697144,
+"objectivity": 0.5448325324058533,
+"__v": 0
+};
 
 const Article = (props) => {
-  const article = props.location.state.article.props;
+
   const [bias, setBias] = useState(false);
   const [sentiment, setSentiment] = useState(false);
   const [objectivity, setObjectivity] = useState(false);
   const [executed, setExecuted] = useState(false);
+  const [articles, setArticles] = useState([]);
 
-  var imgSrc = "../images/sources/" + article.source + ".png";
-  var date = article.date.split("T")[0].split("-");
+  var article = defaultArticle;
+  if(props.location.state){ article = props.location.state.article.props }
+  var id = window.location.pathname.split('/');
+  id = id[id.length - 1];
+  useEffect(() => {
+    axios
+      .get(`${REACT_APP_SERVER_URL}/articles?id=${id}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setArticles(response.data);
+        } else {
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  var articleLoaded = articles.length > 0;
+  if (articleLoaded) {
+    article = articles[0];
+  }
+
+  var imgSrc = "../images/sources/" + (articleLoaded ? article.publisher : article.source) + ".png";
+  var date = (articleLoaded) ? article.firstPublishDate.split("T")[0].split("-") : article.date.split("T")[0].split("-");
   date = new Date(date[0], date[1] - 1, date[2]);
   const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
   const month = new Intl.DateTimeFormat("en", { month: "long" }).format(date);
@@ -150,7 +349,8 @@ const Article = (props) => {
         </div>
         <div>
           <div>
-            By {article.authors.map((author, index) => " " + author)}
+            {/* By {article.authors.map((author, index) => " " + author)} */}
+            By {articleLoaded ? article.contributors.map((author, index) => " " + author) : article.authors.map((author, index) => " " + author)}
           </div>
           <div>{date}</div>
         </div>
